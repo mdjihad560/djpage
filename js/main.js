fb2a4bcbfaf1b4abf64223cd5fccae7eaca09fc3
MD.JIHAD HOSSAIN <43439618+mdjihad560@users.noreply.github.com>
2022-01-31 08:08:05 +0600

    add js

--- js/main.js
+++ js/main.js
@@ -23,38 +23,110 @@ $(function(){
 /*--------------------------------------------------------------
 OS STICKY MENU JS INIT
 --------------------------------------------------------------*/
-$(window).on('scroll', function(){
-  if ($(window).scrollTop() > 50) {
-      $('#sticky-menu').addClass('sticky-menu');
-  } else {
-      $('#sticky-menu').removeClass('sticky-menu');
-  }
+$(".item1").click(function(event){
+    event.preventDefault();
+    $(".item1").hide(300);
+  });
+$(".item2").click(function(event){
+    event.preventDefault();
+    $(".item2").hide(300);
+  });
 
-});
-	
-/*--------------------------------------------------------------
-OS MENU JS
---------------------------------------------------------------*/
+$(".item3").click(function(event){
+    event.preventDefault();
+    $(".item3").hide(300);
+  });
 
-$(".menu-bar").on("click", function () {
-    $(".offcanves-menu, .offcanvas-overlay").addClass("active");
-});
-$(".close, .offcanvas-overlay").on("click", function () {
-    $(".offcanves-menu, .offcanvas-overlay").removeClass("active");
-});
+$(".item4").click(function(event){
+    event.preventDefault();
+    $(".item4").hide(300);
+  });
 
+$(".item5").click(function(event){
+    event.preventDefault();
+    $(".item5").hide(300);
+  });
 
-if ($('.main-menu ul li a').length > 0) {
-    $('.main-menu ul li a').click(function(e){
-      e.preventDefault();
-
-       var target = $(this.getAttribute('href'));
-       
-      $('body , html').animate({
-        scrollTop: target.offset().top,
-      }, 500);
-    });
-  }
+$(".item6").click(function(event){
+    event.preventDefault();
+    $(".item6").hide(300);
+  });
+$(".item7").click(function(event){
+    event.preventDefault();
+    $(".item7").hide(300);
+  });
+$(".item8").click(function(event){
+    event.preventDefault();
+    $(".item8").hide(300);
+  });
+$(".item9").click(function(event){
+    event.preventDefault();
+    $(".item9").hide(300);
+  });
+$(".item10").click(function(event){
+    event.preventDefault();
+    $(".item10").hide(300);
+  });
+$(".item11").click(function(event){
+    event.preventDefault();
+    $(".item11").hide(300);
+  });
+$(".item12").click(function(event){
+    event.preventDefault();
+    $(".item12").hide(300);
+  });
+$(".item13").click(function(event){
+    event.preventDefault();
+    $(".item13").hide(300);
+  });
+$(".item14").click(function(event){
+    event.preventDefault();
+    $(".item14").hide(300);
+  });
+$(".item15").click(function(event){
+    event.preventDefault();
+    $(".item15").hide(300);
+  });
+$(".item16").click(function(event){
+    event.preventDefault();
+    $(".item16").hide(300);
+  });
+$(".item17").click(function(event){
+    event.preventDefault();
+    $(".item17").hide(300);
+  });
+$(".item18").click(function(event){
+    event.preventDefault();
+    $(".item18").hide(300);
+  });
+$(".item19").click(function(event){
+    event.preventDefault();
+    $(".item19").hide(300);
+  });
+$(".item20").click(function(event){
+    event.preventDefault();
+    $(".item20").hide(300);
+  });
+$(".item21").click(function(event){
+    event.preventDefault();
+    $(".item21").hide(300);
+  });
+$(".item22").click(function(event){
+    event.preventDefault();
+    $(".item22").hide(300);
+  });
+$(".item23").click(function(event){
+    event.preventDefault();
+    $(".item23").hide(300);
+  });
+$(".item24").click(function(event){
+    event.preventDefault();
+    $(".item24").hide(300);
+  });
+$(".item25").click(function(event){
+    event.preventDefault();
+    $(".item25").hide(300);
+  });
 
 
 
